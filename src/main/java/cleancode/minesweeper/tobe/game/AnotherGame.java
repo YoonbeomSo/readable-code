0e3d59e0094cf9921e404e98c6bc7d8b4fa4bd7b
YoonbeomSo <ybum1224@naver.com>
2025-04-10 07:29:40 +0900

    ISP(Interface Segregation principle) 원칙

---
+++ src/main/java/cleancode/minesweeper/tobe/game/AnotherGame.java
@@ -0,0 +1,10 @@
+package cleancode.minesweeper.tobe.game;
+
+public class AnotherGame implements GameRunnable {
+
+    @Override
+    public void run() {
+        // Run the game loop
+        System.out.println("Game is running.");
+    }
+}
