e8bb68b764f856ed8de9fa3649674bbc684ec09f
YoonbeomSo <ybum1224@naver.com>
2025-05-06 16:27:10 +0900

    다형성 활용하기

--- src/main/java/cleancode/minesweeper/tobe/cell/CellSnapshot.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/CellSnapshot.java
@@ -36,6 +36,10 @@ public class CellSnapshot {
         return new CellSnapshot(CellSnapshotStatus.UNCHECKED, 0);
     }
 
+    public boolean isSameStatus(CellSnapshotStatus cellSnapshotStatus) {
+        return this.status == cellSnapshotStatus;
+    }
+
     public CellSnapshotStatus getStatus() {
         return status;
     }
@@ -55,4 +59,5 @@ public class CellSnapshot {
     public int hashCode() {
         return Objects.hash(getStatus(), getNearbyLandMineCount());
     }
+
 }
